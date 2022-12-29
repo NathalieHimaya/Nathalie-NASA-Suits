fba9d412c2c8127ab2991d0e741732ade8bb329d
Nathalie Himaya <115034276+NathalieHimaya@users.noreply.github.com>
2022-12-29 02:21:02 -0800

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Security.Cryptography;
-using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+    
 {
-    private float speed = 5.0f;
-    private float turnSpeed;
-    private float horizontalInput;
-    private float verticalInput;
-    private float forwardInput;
-    private float rotationspeed;
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -23,15 +20,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //keep the player in bounds
+  
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        // Moves the car foward based on the vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // Rotates the car based on horizontal input
- 
-        transform.Rotate(Vector3.right, rotationspeed * verticalInput * Time.deltaTime);
-        
-      
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
